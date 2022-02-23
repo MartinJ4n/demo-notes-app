12cecc4950f2686d3583d6e5f5ceb30254162b85
MartinJ4n <martin.j4n@gmail.com>
2022-02-23 18:25:28 +0100

    Adding a storage stack

--- stacks/index.ts
+++ stacks/index.ts
@@ -1,13 +1,6 @@
-import MyStack from "./MyStack";
+import StorageStack from "./StorageStack";
 import * as sst from "@serverless-stack/resources";
 
-export default function main(app: sst.App): void {
-  // Set default runtime for all functions
-  app.setDefaultFunctionProps({
-    runtime: "nodejs14.x"
-  });
-
-  new MyStack(app, "my-stack");
-
-  // Add more stacks
+export default function main(app: sst.App) {
+  new StorageStack(app, "storage");
 }
